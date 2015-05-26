91851781ba9ae14b59c34917c9fcae82d237e43e
Javier Garcia-Vidal Simon <javiergvs5@gmail.com>
2015-05-26 16:28:07 +0200

    Fixed error UI, finish filters map options

--- Interfaz/js/index.js
+++ Interfaz/js/index.js
@@ -19,126 +19,135 @@ function onLocation (position) {
         main(lat, lon);
 }
 
-function main(lat, lon) {
-    cartodb.createVis('map', 'https://javiergvs.cartodb.com/api/v2/viz/f67b93ba-008f-11e5-a9d7-0e0c41326911/viz.json', {
+var map;
+
+function main(lat,lon){
+    map = new L.Map('map', { 
+        center_lat: lat,
+        center_lon: lon,
+        center: [lat,lon],
+        zoom: 5,
+        https: true,
+        mobile_layout: true,
         shareable: false,
         loaderControl: false,
-        https: true,
         title: false,
+        cartodb_logo: false,
         description: false,
         search: false,
         scrollwheel: true,
         tiles_loader: true,
-        detectRetina: true,
-        center_lat: lat,
-        center_lon: lon,
-        zoom: true
-    })
-        .done(function(vis, layers) {
-        // layer 0 is the base layer, layer 1 is cartodb layer
-        // setInteraction is disabled by default
-        layers[1].setInteraction(true);
-        layers[1].on('featureOver', function(e, latlng, pos, data) {
-            cartodb.log.log(e, latlng, pos, data);
-        });
-        // you can get the native map to work with it
-        var map = vis.getNativeMap();
-        $("#progress-bar").css("width", "100%");
-        // now, perform any operations you need
-        // map.setZoom(3);
-        // map.panTo([50.5, 30.5]);
-        //map.panTo([lat, lon]);
-    })
-        .error(function(err) {
-        console.log(err);
+        detectRetina: true
+    });
+
+    L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}@2x.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+    }).addTo(map);
+
+    var layerUrl = 'https://javiergvs.cartodb.com/api/v2/viz/f67b93ba-008f-11e5-a9d7-0e0c41326911/viz.json';
+
+    var sublayers = [];
+    
+    $("#progress-bar").css("width", "100%");
+    cartodb.createLayer(map, layerUrl)
+        .addTo(map)
+        .on('done', function(layer) {
+        // change the query for the first layer
+        var subLayerOptions = {
+            sql: "SELECT * FROM map",
+            cartocss: "#map {marker-fill-opacity: 0.9;marker-line-color: #555555;marker-line-width: 1.5;marker-line-opacity: 1;marker-placement: point;marker-type: ellipse;marker-width: 10;marker-allow-overlap: true;} #map[type='Arte'] {marker-fill: #8d6e63;} #map[type='Deportivo'] {marker-fill: #229A00;} #map[type='Educativo'] {marker-fill: #FF9900;} #map[type='Infantil'] {marker-fill: #A53ED5;} #map[type='Musica'] {marker-fill: #F84F40;} #map[type='Ocio'] {marker-fill: #3E7BB6;} #map[type=null] {marker-fill: #aaaaaa;}"
+        };
+
+        var sublayer = layer.getSubLayer(0);
+
+        sublayer.set(subLayerOptions);
+
+        sublayers.push(sublayer);
+    }).on('error', function() {
+        //log the error
+    });
+
+    //we define the queries that will be performed when we click on the buttons, by modifying the SQL of our layer
+    var LayerActions = {
+        default: function(){
+            sublayers[0].setSQL("SELECT * FROM map");
+            return true;
+        },
+        clean: function(){
+            sublayers[0].setSQL("SELECT * FROM map WHERE type IS NOT NULL");
+            return true;
+        },
+        music: function(){
+            sublayers[0].setSQL("SELECT * FROM map WHERE type IN ('Musica') AND type IS NOT NULL");
+            return true;
+        },
+        sports: function(){
+            sublayers[0].setSQL("SELECT * FROM map WHERE type IN ('Deportivo') AND type IS NOT NULL");
+            return true;
+        },
+        art: function(){
+            sublayers[0].setSQL("SELECT * FROM map WHERE type IN ('Arte') AND type IS NOT NULL");
+            return true;
+        },
+        leisure: function(){
+            sublayers[0].setSQL("SELECT * FROM map WHERE type IN ('Ocio') AND type IS NOT NULL");
+            return true;
+        },
+        infantile: function(){
+            sublayers[0].setSQL("SELECT * FROM map WHERE type IN ('Infantil') AND type IS NOT NULL");
+            return true;
+        },
+        educative: function(){
+            sublayers[0].setSQL("SELECT * FROM map WHERE type IN ('Educativo') AND type IS NOT NULL");
+            return true;
+        },
+        tecnology: function(){
+        sublayers[0].setSQL("SELECT * FROM map WHERE type IN ('Tecnologia') AND type IS NOT NULL");
+            return true;
+        },
+        games: function(){
+            sublayers[0].setSQL("SELECT * FROM map WHERE type IN ('Juegos') AND type IS NOT NULL");
+            return true;
+        },
+        business: function(){
+            sublayers[0].setSQL("SELECT * FROM map WHERE type IN ('Empresas') AND type IS NOT NULL");
+            return true;
+        },
+        menus: function(){
+            sublayers[0].setSQL("SELECT * FROM map WHERE type IN ('Menus') AND type IS NOT NULL");
+            return true;
+        },
+        sale: function(){
+            sublayers[0].setSQL("SELECT * FROM map WHERE type IN ('Ofertas') AND type IS NOT NULL");
+            return true;
+        },
+        popularity: function(){
+            sublayers[0].setSQL("SELECT * FROM map WHERE affiliation >= 20000 AND type IS NOT NULL LIMIT 20");
+        return true;  
+        },
+        capacity: function(){
+            sublayers[0].setSQL("SELECT * FROM map WHERE capacity >= 20000 AND type IS NOT NULL LIMIT 20");
+            return true;  
+        },
+        date: function(){
+            sublayers[0].setSQL("SELECT * FROM map WHERE (date >= ('2014-07-11T16:46:40+00:00')) LIMIT 10");
+            return true;  
+        },
+        location: function(){
+            sublayers[0].setSQL("SELECT * FROM map ORDER BY the_geom <-> ST_SetSRID(ST_MakePoint(lon,lat),4326) LIMIT 10");
+        return true;  
+    }
+        
+    };
+    $('.button-filter').click(function() {
+        $('.button-filter').removeClass('active');
+        $(this).parent('li').addClass('active');
+        LayerActions[$(this).attr('id')]();
+    });
+    
+    $('.button-menu').click(function() {
+        LayerActions['default']();
     });
 }
+
 window.onload = getLocation;
-//window.onload = main;
-
-//####### GET EVENTS & POST EVENT ###################################################################################################
-$(document).ready(function(){
-    function onSubmit (event) {
-        event.preventDefault();
-        console.debug('SUBMITTED');
-        var newCharacter = {
-            name: $('#name').val(),
-            occupation: $('#occupation').val(),
-            weapon: $('#weapon').val()
-            // Build a new character from the values in the form
-        }
-
-        // Send a post request with the data for the new character
-        var request = $.post('https://ironhack-characters.herokuapp.com/characters', newCharacter);
-
-        function onSaveSuccess (response) {
-            console.debug('BOOM', response);
-        }
-
-        function onSaveFailure (err) {
-            console.error(err.responseJSON);
-        }
-
-        request.done(onSaveSuccess);
-        request.fail(onSaveFailure);
-    }
-
-    $('.js-submit').on('click', onSubmit);
-
-    
-    function getEventExplore () {
-        var request = $.get("https://javiergvs.cartodb.com/api/v2/sql?q=SELECT * FROM map WHERE type IS NOT NULL");
-
-        function getEvents (explore_events) {
-            console.log('REQUEST DONE',explore_events);
-            var num = 0;
-            var picture = 0;
-            var picture_route = ['city','nightlife','technics','fashion','business','transport','sports',
-                                 'people','abstract','animals','cats','food','nature'];
-            $.each(explore_events.rows, function(id, event) {
-                
-                if (num >= 10) {
-                    num = 0;
-                    picture++;
-                } else
-                    num++;
-
-                var html = [
-                    '<div class="col l3 m6 s12">',
-                        '<div class="card small z-depth-2">',
-                            '<div class="card-image position-center waves-effect waves-block waves-light">',
-                    '<img class="activator" src="http://lorempixel.com/400/200/' + picture_route[picture] + '/' + num + '">',
-                            '</div>',
-                            '<div class="card-content truncate">',
-                                '<span class="card-title activator grey-text text-darken-4">' + event.name + '<p>',
-                                '<a href="#">Leer mas</a></p></span>',
-                            '</div>',
-                            '<div class="card-reveal">',
-                                '<span class="card-title grey-text text-darken-4">' + event.name + '<i class="mdi-navigation-close right">',
-                                '</i></span>',
-                                '<p><i class="mdi-action-home prefix fix-prefix-position"></i>Place : <b>'+ event.place +'</b></p>',
-                            '<p><i class="mdi-action-grade prefix fix-prefix-position"></i>Affilation : <b>'+  event.affiliation +'</b>',
-                                '</p>',
-                                '<p><i class="mdi-device-access-time prefix fix-prefix-position"></i>Date : <b>'+ event.date +'</b></p>',
-                                '<p><i class="mdi-social-public prefix fix-prefix-position"></i>Type : <b>'+ event.type +'</b></p>',
-                                '<blockquote class="text-justify">'+ event.description +'</blockquote>',
-                            '</div>',
-                        '</div>',
-                    '</div>'
-                    ].join('\n');
-
-                $('#print-event-explore').append(html);
-            });
-        }
-
-        function eventError (err1, err2, err3) {
-            console.error('Error!!', err1, err2, err3);
-        }
-
-        request.done(getEvents);
-        request.fail(eventError);
-    }
-
-    $('#explore-button').on('click', getEventExplore);
-});
-
